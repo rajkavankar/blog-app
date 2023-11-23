9d5b85201b40244065f97041a19ed1e6e89fccd1
Raj kavankar <rajkavankar@gmail.com>
2023-11-23 18:39:47 +0530

    Redux toolkit setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react"
+import { Outlet } from "react-router-dom"
+import { Header, Footer } from "./components"
 
 const App = () => {
 	return (
-		<div>
-			<h1 className="text-3xl">Hello</h1>
+		<div className="min-h-screen flex flex-wrap content-between bg-slate-800 text-slate-100">
+			<Header />
+			<main>
+				<Outlet />
+			</main>
+			<Footer />
 		</div>
 	)
 }
